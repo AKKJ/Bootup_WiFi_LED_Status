cffa7b1eafebde849ee1dc52592164adb584d501
AKKJ <ajaykkjoseph@gmail.com>
2022-10-26 11:13:53 +0100

    First commit

--- WiFiStatus.sh
+++ WiFiStatus.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
 
-
-# re = '^[0-9]+$'
-# if [[ $iwgetid =~ $re ]]; then
-#    echo "aadfadsf"; #exit 1
-# fi
-
-# while :
-# do 
-#    #https://stackoverflow.com/questions/617182/how-can-i-suppress-all-output-from-a-command-using-bash
-#     : $(echo 255 |sudo tee /sys/class/leds/led0/brightness) 
-#     #https://www.freecodecamp.org/news/bash-sleep-how-to-make-a-shell-script-wait-n-seconds-example-command/#:~:text=How%20to%20Use%20the%20Bash,or%20a%20floating%20point%20number.
-#     sleep 0.5
-#     : $(echo 0 |sudo tee /sys/class/leds/led0/brightness)
-#     sleep 2
-# done
-
 wget -q --spider http://google.com
 
 while :
