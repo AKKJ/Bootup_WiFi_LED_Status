3674bbdadcc354bdba85565c44e13419ec381135
AKKJ <ajaykkjoseph@gmail.com>
2022-10-26 22:49:18 +0100

    Inverted LEDs
    The inbuilt RED light turns on because CRONTAB does not support Hostname -I. By doing this the else statement becomes pointless. However Green light blinks when connected to a WiFi network.

--- WiFiStatus.sh
+++ WiFiStatus.sh
@@ -1,27 +1,29 @@
-#!/bin/bash
 
+_IP=$(hostname -I) || true
 
-wget -q --spider http://google.com
 
 while :
-do 
+do
 
-   if [ $? -eq 0 ]; then
-      # echo "Online"
-      #https://stackoverflow.com/questions/617182/how-can-i-suppress-all-output-from-a-command-using-bash
-      : $(echo 255 |sudo tee /sys/class/leds/led0/brightness) 
-      #https://www.freecodecamp.org/news/bash-sleep-how-to-make-a-shell-script-wait-n-seconds-example-command/#:~:text=How%20to%20Use%20the%20Bash,or%20a%20floating%20point%20number.
-      sleep 0.5
-      : $(echo 0 |sudo tee /sys/class/leds/led0/brightness)
-      sleep 2      
-   else
-      # echo "Offline"
-      #https://stackoverflow.com/questions/617182/how-can-i-suppress-all-output-from-a-command-using-bash
-      : $(echo 255 |sudo tee /sys/class/leds/led1/brightness) 
-      #https://www.freecodecamp.org/news/bash-sleep-how-to-make-a-shell-script-wait-n-seconds-example-command/#:~:text=How%20to%20Use%20the%20Bash,or%20a%20floating%20point%20number.
-      sleep 0.5
-      : $(echo 0 |sudo tee /sys/class/leds/led1/brightness)
-      sleep 2            
-   fi
+    if [ "$_IP" ]; then
+        echo "If loop"
+        # printf "My IP address is %s\n" "$_IP"
+        # echo "Online"
+        #https://stackoverflow.com/questions/617182/how-can-i-suppress-all-output-from-a-command-using-bash
+        : $(echo 255 |sudo tee /sys/class/leds/led1/brightness) 
+        #https://www.freecodecamp.org/news/bash-sleep-how-to-make-a-shell-script-wait-n-seconds-example-command/#:~:text=How%20to%20Use%20the%20Bash,or%20a%20floating%20point%20number.
+        sleep 0.5
+        : $(echo 0 |sudo tee /sys/class/leds/led1/brightness)
+        sleep 0.5
+    else
+        # echo "Offline"
+        #https://stackoverflow.com/questions/617182/how-can-i-suppress-all-output-from-a-command-using-bash
+        : $(echo 255 |sudo tee /sys/class/leds/led0/brightness) 
+        #https://www.freecodecamp.org/news/bash-sleep-how-to-make-a-shell-script-wait-n-seconds-example-command/#:~:text=How%20to%20Use%20the%20Bash,or%20a%20floating%20point%20number.
+        sleep 1
+        : $(echo 0 |sudo tee /sys/class/leds/led0/brightness)
+        sleep 1       
+    fi
+done     
 
-done
+
