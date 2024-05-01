530781ae43521d0e0f56e73211f970714c844f8e
AKKJ <94000644+AKKJ@users.noreply.github.com>
2024-05-01 20:49:10 +0100

    Update WiFiStatus.sh
    Working code with example from stackexchange.com

--- WiFiStatus.sh
+++ WiFiStatus.sh
@@ -1,3 +1,12 @@
+# check if /sys/class/leds has LED0 or LED1 or PWR folder. 
+# see the following link - https://raspberrypi.stackexchange.com/questions/143740/missing-sys-class-leds-led0-and-sys-class-leds-led1-directories-on-pi3-model-b
+  # In this example the sh script need ON or OFF command when executing "./onBoardLEDBlink.sh ON"
+
+# run the following command 
+  # In this case the file is saved as onBoardLEDBlinkIP.sh
+  # sudo chmod +x onBoardLEDBlinkIP.sh
+  # ./onBoardLEDBlinkIP.sh
+
 
 _IP=$(hostname -I) || true
 
@@ -10,20 +19,19 @@ do
         # printf "My IP address is %s\n" "$_IP"
         # echo "Online"
         #https://stackoverflow.com/questions/617182/how-can-i-suppress-all-output-from-a-command-using-bash
-        : $(echo 255 |sudo tee /sys/class/leds/led1/brightness) 
+        : $(echo 255 |sudo tee /sys/class/leds/PWR/brightness)
         #https://www.freecodecamp.org/news/bash-sleep-how-to-make-a-shell-script-wait-n-seconds-example-command/#:~:text=How%20to%20Use%20the%20Bash,or%20a%20floating%20point%20number.
         sleep 0.5
-        : $(echo 0 |sudo tee /sys/class/leds/led1/brightness)
+        : $(echo 0 |sudo tee /sys/class/leds/PWR/brightness)
         sleep 0.5
     else
         # echo "Offline"
         #https://stackoverflow.com/questions/617182/how-can-i-suppress-all-output-from-a-command-using-bash
-        : $(echo 255 |sudo tee /sys/class/leds/led0/brightness) 
+        : $(echo 255 |sudo tee /sys/class/leds/PWR/brightness)
         #https://www.freecodecamp.org/news/bash-sleep-how-to-make-a-shell-script-wait-n-seconds-example-command/#:~:text=How%20to%20Use%20the%20Bash,or%20a%20floating%20point%20number.
         sleep 1
-        : $(echo 0 |sudo tee /sys/class/leds/led0/brightness)
-        sleep 1       
+        : $(echo 0 |sudo tee /sys/class/leds/PWR/brightness)
+        sleep 1
     fi
-done     
-
+done
 
